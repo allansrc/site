3bffbb34703385c583c5e5c59e3e03f71898dec1
Allan Ramos <allansrc@fedoraproject.org>
2023-12-18 23:43:14 -0300

    add global values sizes

--- lib/shared/constants/sizes.dart
+++ lib/shared/constants/sizes.dart
@@ -3,4 +3,12 @@ import 'package:flutter/material.dart';
 class AppSizes {
   static bool hasWebMinSize(BuildContext context) =>
       MediaQuery.of(context).size.width >= 690.0;
+
+  static const double galleryHeaderHeight = 64;
+
+  static const double desktopDisplay1FontDelta = 16;
+
+  static const double desktopSettingsWidth = 520;
+
+  static const double systemTextScaleFactorOption = -1;
 }
